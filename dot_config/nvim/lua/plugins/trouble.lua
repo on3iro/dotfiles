cd665a99bfebbf39e818ba1cc266c3f45b444901
Theo Salzmann <theo.salzmann@sandstorm.de>
2024-10-29 12:27:10 +0100

    Add markdown rendering, trouble config improvements

--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,18 +1,18 @@
 return {
-	"folke/trouble.nvim",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	cmd = "Trouble",
-	opts = {},
-	keys = {
-		{
-			"<leader>di",
-			"<cmd>Trouble diagnostics toggle<cr>",
-			desc = "Diagnostics (Trouble)",
-		},
-		{
-			"<leader>dq",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
-		},
-	},
+  "folke/trouble.nvim",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  cmd = "Trouble",
+  opts = {},
+  keys = {
+    {
+      "<leader>di",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>dq",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
