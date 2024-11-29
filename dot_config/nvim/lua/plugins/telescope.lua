cc4d65b8ccb5f14194c66b8e7e0cd2c3c3e487d9
Theo Salzmann <theo.salzmann@sandstorm.de>
2024-11-29 09:45:08 +0100

    nvim: Started overhauling telescope config

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,146 @@
+return {
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "BurntSushi/ripgrep",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    config = function()
+      local a = vim.api
+
+      local previewers = require("telescope.previewers")
+
+      -- Ignore files bigger then threshol (e.g. images)
+      local new_maker = function(filepath, bufnr, opts)
+        opts = opts or {}
+
+        filepath = vim.fn.expand(filepath)
+        vim.loop.fs_stat(filepath, function(_, stat)
+          if not stat then
+            return
+          end
+          if stat.size > 100000 then
+            return
+          else
+            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+          end
+        end)
+      end
+
+      actions = require("telescope.actions")
+
+      require("telescope").setup({
+        -- ...
+        defaults = {
+          wrap_results = true,
+          buffer_previewer_maker = new_maker,
+          file_ignore_patterns = { "node_modules", "%.git/" },
+          path_display = {
+            shorten = true,
+            -- truncate = 3,
+          },
+          -- Old style arguments as a quickfix for ripgrep memory leak
+          -- see: https://github.com/nvim-telescope/telescope.nvim/issues/2482#issuecomment-1528053505
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+          },
+          mappings = {
+            i = {
+              ["<c-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+            },
+          },
+        },
+        pickers = {
+          find_files = { theme = "dropdown" },
+          grep_string = { theme = "dropdown" },
+          buffers = { theme = "dropdown" },
+          live_grep = { theme = "dropdown" },
+          current_buffer_fuzzy_find = { theme = "dropdown" },
+          oldfiles = { theme = "dropdown" },
+          quickfix = { theme = "dropdown", fname_width = 80 },
+          lsp_references = { theme = "dropdown" }, -- Add this line
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({
+              -- even more opts
+            }),
+
+            -- pseudo code / specification for writing custom displays, like the one
+            -- for "codeactions"
+            -- specific_opts = {
+            --   [kind] = {
+            --     make_indexed = function(items) -> indexed_items, width,
+            --     make_displayer = function(widths) -> displayer
+            --     make_display = function(displayer) -> function(e)
+            --     make_ordinal = function(e) -> string
+            --   },
+            --   -- for example to disable the custom builtin "codeactions" display
+            --      do the following
+            --   codeactions = false,
+            -- }
+          },
+        },
+      })
+
+      require("telescope").load_extension("fzf")
+      require("telescope").load_extension("ui-select")
+
+      require("plugins.flutter.telescope").setup()
+
+      -- Files
+      a.nvim_set_keymap("n", "<leader>f", "<cmd>Telescope find_files hidden=true<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>nw", "<cmd>Telescope grep_string<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>bu", "<cmd>Telescope buffers<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>/", "<cmd>Telescope live_grep<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>l", "<cmd>Telescope current_buffer_fuzzy_find<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>hi", "<cmd>Telescope oldfiles<cr>", { noremap = false })
+      local builtin = require("telescope.builtin")
+
+      -- Notes
+      vim.keymap.set("n", "<leader>n/", function()
+        builtin.live_grep({
+          additional_args = {
+            "-L", -- Follow symbolic links
+          },
+          search_dirs = {
+            "~/notes",
+          },
+        })
+      end, { noremap = false })
+      vim.keymap.set("n", "<leader>nf", function()
+        builtin.find_files({
+          follow = true, -- Follow symbolic links
+          search_dirs = {
+            "~/notes",
+          },
+        })
+      end, { noremap = false })
+
+      vim.keymap.set("n", "<leader>nt", "<cmd>lua live_grep_frontmatter_tags()<cr>", { noremap = true })
+
+      -- Git
+      a.nvim_set_keymap("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { noremap = false })
+      a.nvim_set_keymap("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { noremap = false })
+
+      -- Registers
+      a.nvim_set_keymap("n", "<leader>re", "<cmd>Telescope registers<cr>", { noremap = false })
+
+      -- Marks
+      a.nvim_set_keymap("n", "<leader>ma", "<cmd>Telescope marks<cr>", { noremap = false })
+    end,
+  },
+
+  -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+}
