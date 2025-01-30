245125e0582021752bffc219ef5bb037796b904d
Theo Salzmann <theo.salzmann@sandstorm.de>
2025-01-30 13:20:12 +0100

    nvim: yamlls

--- dot_config/nvim/lua/plugins/lsp/servers.lua
+++ dot_config/nvim/lua/plugins/lsp/servers.lua
@@ -4,40 +4,47 @@
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 return {
-	gopls = {
-			hints = {
-				assignVariableTypes = true,
-				compositeLiteralTypes = true,
-				constantValues = true,
-				functionTypeParameters = true,
-				parameterNames = true,
-				rangeVariableTypes = true,
-			},
-	},
-	rust_analyzer = {
-		["rust-analyzer"] = {
-			-- enable clippy on save
-			cargo = {
-				autoreload = true,
-			},
-			checkOnSave = {
-				command = "clippy",
-				enable = true,
-			},
-			completion = {
-				autoimport = {
-					enable = true,
-				},
-			},
-			diagnostics = {
-				disabled = { "macro-error" },
-			},
-			procMacro = {
-				enable = true,
-			},
-			updates = {
-				channel = "stable",
-			},
-		},
-	},
+  gopls = {
+    hints = {
+      assignVariableTypes = true,
+      compositeLiteralTypes = true,
+      constantValues = true,
+      functionTypeParameters = true,
+      parameterNames = true,
+      rangeVariableTypes = true,
+    },
+  },
+  rust_analyzer = {
+    ["rust-analyzer"] = {
+      -- enable clippy on save
+      cargo = {
+        autoreload = true,
+      },
+      checkOnSave = {
+        command = "clippy",
+        enable = true,
+      },
+      completion = {
+        autoimport = {
+          enable = true,
+        },
+      },
+      diagnostics = {
+        disabled = { "macro-error" },
+      },
+      procMacro = {
+        enable = true,
+      },
+      updates = {
+        channel = "stable",
+      },
+    },
+  },
+  yaml = {
+    schemas = {
+      ["https://raw.githubusercontent.com/sandstorm/Sandstorm.CookiePunch/d3ee75e8a72ca67c917d13bf166044422feb88f2/schema.json"] =
+      "Settings.CookiePunch.y*ml",
+      -- ["file://home/theo/.config/nvim/schemas/cookiepunch.json"] = "Settings.CookiePunch.y*ml"
+    }
+  }
 }
