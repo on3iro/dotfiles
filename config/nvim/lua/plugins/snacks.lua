603dff6d8b9f9a1f8f80053e215f69dc2e986bb9
Theo Salzmann <theo.salzmann@sandstorm.de>
2025-03-21 10:27:13 +0100

    nvim: Some snacks settings

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -70,11 +70,49 @@ return {
         },
       },
     },
+    zen = {
+      toggles = {
+        dim = true,
+        git_signs = false,
+        mini_diff_signs = false,
+        -- diagnostics = false,
+        -- inlay_hints = false,
+      },
+      show = {
+        statusline = false, -- can only be shown when using the global statusline
+        tabline = false,
+      },
+      ---@type snacks.win.Config
+      win = { style = "zen" },
+      --- Callback when the window is opened.
+      ---@param win snacks.win
+      on_open = function(win) end,
+      --- Callback when the window is closed.
+      ---@param win snacks.win
+      on_close = function(win) end,
+      --- Options for the `Snacks.zen.zoom()`
+      ---@type snacks.zen.Config
+      zoom = {
+        toggles = {
+          dim = true,
+        },
+        show = { statusline = true, tabline = true },
+        win = {
+          backdrop = false,
+          width = 0, -- full width
+        },
+      },
+    },
+    styles = {}
   },
   keys = {
     -- Scratch buffers
     { "<leader>.",   function() end,                                       desc = "Toggle Scratch Buffer" },
     { "<leader>S",   function() Snacks.scratch.select() end,               desc = "Select Scratch Buffer" },
+
+    -- Zen
+    { "<leader>zen", function() Snacks.zen() end,                          desc = "Zen Mode" },
+    { "<leader>+",   function() Snacks.zen.zoom() end,                     desc = "Zen Zoom" },
 
     ------------
     -- Picker --
@@ -131,5 +169,5 @@ return {
       end,
       desc = "Find Notes"
     }
-  }
+  },
 }
