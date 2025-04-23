8a919574517a4452696b028aa412d2941ab3a1cc
Theo Salzmann <theo.salzmann@sandstorm.de>
2025-04-23 09:29:14 +0200

    neovim: Adjust codecompanion config

--- config/nvim/lua/plugins/codecompanion.ai.lua
+++ config/nvim/lua/plugins/codecompanion.ai.lua
@@ -5,8 +5,12 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   keys = {
+    -- Toggle the CodeCompanion Chat interface
     { "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>", desc = "CodeCompanion Chat" },
+    -- Open the CodeCompanion Actions menu
     { "<leader>ac", "<cmd>CodeCompanionActions<cr>",     desc = "CodeCompanion Actions" },
+    -- Add the visually selected text to CodeCompanion Chat (visual mode only)
+    { "ga",         "<cmd>CodeCompanionChat Add<cr>",    mode = "v",                    desc = "CodeCompanion Add Selection" },
   },
   opts = {
     strategies = {
