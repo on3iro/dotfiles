9462ae4509b64e21dd5ab61f7a5f3b4c5dafb6cb
Theo Salzmann <theo.salzmann@sandstorm.de>
2025-05-17 19:30:16 +0200

    nvim: Fix Mason config

--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -27,24 +27,12 @@ return {
       require("mason").setup()
 
       -- Ensure the servers above are installed
-      local mason_lspconfig = require("mason-lspconfig")
-
-      mason_lspconfig.setup({
+      require("mason-lspconfig").setup({
         ensure_installed = {
           "templ",
           "marksman",
           "intelephense"
         },
-      })
-
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-          })
-        end,
       })
 
       require("lspconfig.configs").vtsls = require("vtsls")
