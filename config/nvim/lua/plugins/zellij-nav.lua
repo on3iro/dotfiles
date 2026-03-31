5cd1ce7fd7eda7eec4d82c8b6c32dcf091cd292a
Theo Salzmann <theo.salzmann@sandstorm.de>
2026-03-31 10:22:31 +0200

    nvim: Change to new zellij integration plugin
    This currently introduces some delay, but at least it works

--- config/nvim/lua/plugins/zellij-nav.lua
+++ config/nvim/lua/plugins/zellij-nav.lua
@@ -1,14 +1,24 @@
 return {
   {
-    "https://git.sr.ht/~swaits/zellij-nav.nvim",
-    lazy = true,
-    event = "VeryLazy",
+    "mrjones2014/smart-splits.nvim",
+    lazy = false,
     keys = {
-      { "<c-h>", "<cmd>ZellijNavigateLeft<cr>",  { silent = true, desc = "navigate left or tab" } },
-      { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down" } },
-      { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up" } },
-      { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right or tab" } },
+      { "<c-h>", function() require("smart-splits").move_cursor_left() end,  { silent = true, desc = "navigate left" } },
+      { "<c-j>", function() require("smart-splits").move_cursor_down() end,  { silent = true, desc = "navigate down" } },
+      { "<c-k>", function() require("smart-splits").move_cursor_up() end,    { silent = true, desc = "navigate up" } },
+      { "<c-l>", function() require("smart-splits").move_cursor_right() end, { silent = true, desc = "navigate right" } },
     },
-    opts = {},
-  }
+  },
+  -- {
+  --   "https://git.sr.ht/~swaits/zellij-nav.nvim",
+  --   lazy = true,
+  --   event = "VeryLazy",
+  --   keys = {
+  --     { "<c-h>", "<cmd>ZellijNavigateLeft<cr>",  { silent = true, desc = "navigate left or tab" } },
+  --     { "<c-j>", "<cmd>ZellijNavigateDown<cr>",  { silent = true, desc = "navigate down" } },
+  --     { "<c-k>", "<cmd>ZellijNavigateUp<cr>",    { silent = true, desc = "navigate up" } },
+  --     { "<c-l>", "<cmd>ZellijNavigateRight<cr>", { silent = true, desc = "navigate right or tab" } },
+  --   },
+  --   opts = {},
+  -- }
 }
